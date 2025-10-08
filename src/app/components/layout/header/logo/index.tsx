a38b96b9be829cc45fd7ed953dcea1a2d13877b8
Serge <info.divvyfi@gmail.com>
2025-10-08 16:04:16 -0400

    Update index.tsx

--- src/app/components/layout/header/logo/index.tsx
+++ src/app/components/layout/header/logo/index.tsx
@@ -3,14 +3,20 @@ import Link from "next/link";
 
 const Logo: React.FC = () => {
   return (
-    <Link href="/">
+    <Link href="/" style={{ display: "inline-block" }}>
       <Image
-        src="/images/logo/logo.svg"
-        alt="logo"
-        width={160}
-        height={50}
-        style={{ width: "auto", height: "auto" }}
+        src="/images/divvyfi-logo.png"
+        alt="DivvyFi Logo"
+        width={250}
+        height={100}
+        priority
         quality={100}
+        style={{
+          width: "auto",
+          height: "auto",
+          maxWidth: "250px",
+          display: "block",
+        }}
       />
     </Link>
   );
