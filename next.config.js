52ece8381128881f968860da7286741447e7ce0b
Serge <info.divvyfi@gmail.com>
2025-10-08 21:40:47 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    appDir: true, // ensures Next knows you’re using the app router
+  },
 };
 
 module.exports = nextConfig;
