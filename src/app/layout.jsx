15427876ff9221e39034bdc69c7a558c3450af41
Serge <info.divvyfi@gmail.com>
2025-11-02 18:19:30 -0500

    Create RootLayout component for application layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,1 +1,9 @@
+// src/app/layout.jsx
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-[#0a0a0f] text-white">{children}</body>
+    </html>
+  )
+}
 
