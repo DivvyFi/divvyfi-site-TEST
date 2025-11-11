410ca31ac999356d9a7911746d49621d697a05b5
Serge <info.divvyfi@gmail.com>
2025-11-11 14:14:45 -0500

    Delete src/app/layout.jsx

--- src/app/layout.jsx
+++
@@ -1,9 +0,0 @@
-// src/app/layout.jsx
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="bg-[#0a0a0f] text-white">{children}</body>
-    </html>
-  )
-}
-
